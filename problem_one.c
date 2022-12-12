a9d224370e6afc945dc5636f44f4e34b55f3086a
theMorshed <themorshedctg@gmail.com>
2022-12-12 21:41:52 +0600

    problem one solved

---
+++ problem_one.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int calculate_power(int n, int m) ;
+
+int main() 
+{
+    int n, m, result = 1;
+    scanf("%d %d", &n, &m);
+    result *= calculate_power(n, m);
+    printf("%d\n", result);
+    
+    return 0;
+}
+
+int calculate_power(int n, int m) 
+{
+    if (m == 0) {
+        return 1;
+    }
+    return n * calculate_power(n, m - 1);
+}
